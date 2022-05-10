f3a41c5ecaabfc4ae41c6a6d4da2b2b43731d175
Nicolas Aguirre Yacup <nicolas.aguirre.y@gmail.com>
2022-05-09 22:19:48 -0500

    archivos curso

---
+++ src/angular_install_guide.md
@@ -0,0 +1,19 @@
+#Comandos de instalacion para ANGULAR
+##Crear proyecto: 
+```
+ng new my-project
+
+```
+##Lanzar servidor de desarrollo: 
+```javascript
+ng serve
+```
+##Lanzar servidor de desarrollo y que abra el navegador automáticamente:
+```javascript
+ng serve -o
+```
+##Lanzar el servidor en un puerto especifico: 
+```javascript
+ng serve -o --port=3500
+```
+- Si lanzamos el comando ng version desde la carpeta del proyecto podremos obtener mayor detalle de las tecnologias utilizadas.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,93 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+  name = 'Nico'
+  age = '26'
+  img = 'https://source.unsplash.com/random'
+  activator = true
+  person = {
+    name: 'Niko',
+    age: 95,
+    img: 'https://source.unsplash.com/random'
+  }
+
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  }
+
+  toggleButtons(){
+    this.activator = !this.activator
+  }
+
+  add_age(){
+    this.person.age += 10
+  }
+
+  rest_age(){
+    this.person.age -= 10
+  }
+
+  change_name(event: Event){
+    const element = event.target as HTMLInputElement
+    this.person.name = element.value
+  }
+
+  my_list: string[] = ['casa','carro']
+  new_item: string = ''
+
+  add_to_list(){
+    this.my_list.push(this.new_item)
+    this.new_item=''
+  }
+
+  remove_from_list(index: number){
+    this.my_list.splice(index,1)
+  }
+
+  register = {
+    name: '',
+    email: '',
+    password: ''
+  }
+  onRegister(){
+    console.log(this.register)
+  }
+
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: 'https://source.unsplash.com/random',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: 'https://source.unsplash.com/random'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: 'https://source.unsplash.com/random'
+    }
+  ]
+
 }
